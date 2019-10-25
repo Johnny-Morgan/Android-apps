4575c013d8274180084f3806f1af0ef5501ceec9
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-10-25 23:42:46 +0100

    Initial commit

---
+++ Connect3Game/MainActivity.java
@@ -0,0 +1,36 @@
+package dev.johnmorgan.connect3game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // 0: yellow, 1: red
+    int activePlayer = 0;
+
+    public void dropIn(View view) {
+
+        ImageView counter = (ImageView) view;
+        counter.setTranslationY(-1500);
+
+        if (activePlayer == 0) {
+            counter.setImageResource(R.drawable.yellow);
+            activePlayer = 1;
+        } else {
+            counter.setImageResource(R.drawable.red);
+            activePlayer = 0;
+        }
+
+        counter.animate().translationYBy(1500).rotationBy(1800).alpha(1);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
