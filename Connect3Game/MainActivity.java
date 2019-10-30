7ee2c130fbf6a8b06f155aa9231f3abd175831f5
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-10-30 23:47:16 +0000

    Bug fixes

--- Connect3Game/MainActivity.java
+++ Connect3Game/MainActivity.java
@@ -3,10 +3,9 @@ package dev.johnmorgan.connect3game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.GridLayout;
+import androidx.gridlayout.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,23 +13,21 @@ public class MainActivity extends AppCompatActivity {
 
     // 0: yellow, 1: red, 2: empty
     int activePlayer = 0;
-    int[] gamestate = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
     int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
     boolean gameOver = false;
 
     public void dropIn(View view) {
-
         ImageView counter = (ImageView) view;
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        if (!gameOver && gamestate[tappedCounter] == 2) {
+        if (!gameOver && gameState[tappedCounter] == 2) {
             counter.setTranslationY(-1500);
-            gamestate[tappedCounter] = activePlayer;
+            gameState[tappedCounter] = activePlayer;
 
             if (activePlayer == 0) {
                 counter.setImageResource(R.drawable.yellow);
                 activePlayer = 1;
-                Log.i("tag", counter.getTag().toString());
             } else {
                 counter.setImageResource(R.drawable.red);
                 activePlayer = 0;
@@ -38,8 +35,8 @@ public class MainActivity extends AppCompatActivity {
 
             counter.animate().translationYBy(1500).rotationBy(1800).alpha(1);
             for (int[] winningPosition : winningPositions) {
-                if (gamestate[winningPosition[0]] == gamestate[winningPosition[1]] && gamestate[winningPosition[1]] == gamestate[winningPosition[2]]
-                        && gamestate[winningPosition[0]] != 2) {
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]]
+                        && gameState[winningPosition[0]] != 2) {
                     String message;
                     if (activePlayer == 1)
                         message = "Yellow";
@@ -63,15 +60,17 @@ public class MainActivity extends AppCompatActivity {
         Button playAgainButton = findViewById(R.id.playAgainButton);
         winnerTextView.setVisibility(View.INVISIBLE);
         playAgainButton.setVisibility(View.INVISIBLE);
+
         gameOver = false;
         activePlayer = 0;
-        for (int i = 0; i < gamestate.length; i++) {
-            gamestate[i] = 2;
+
+        for (int i = 0; i < gameState.length; i++) {
+            gameState[i] = 2;
         }
 
         for (int i = 0; i < grid.getChildCount(); i++) {
             ImageView counter = (ImageView) grid.getChildAt(i);
-            counter.setImageDrawable(null);
+            counter.setAlpha(0.0f);
         }
     }
 
