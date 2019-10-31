ef526b6a8be8b139cce1d607b03d4f045e2b6afd
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-10-31 00:28:03 +0000

    Added code to determine drawn game

--- Connect3Game/MainActivity.java
+++ Connect3Game/MainActivity.java
@@ -16,10 +16,14 @@ public class MainActivity extends AppCompatActivity {
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
     int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
     boolean gameOver = false;
+    static int counterCount;
 
     public void dropIn(View view) {
+        counterCount++;
         ImageView counter = (ImageView) view;
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
+        TextView winnerTextView = findViewById(R.id.winnerTextView);
+        Button playAgainButton = findViewById(R.id.playAgainButton);
 
         if (!gameOver && gameState[tappedCounter] == 2) {
             counter.setTranslationY(-1500);
@@ -32,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
                 counter.setImageResource(R.drawable.red);
                 activePlayer = 0;
             }
-
+            
             counter.animate().translationYBy(1500).rotationBy(1800).alpha(1);
             for (int[] winningPosition : winningPositions) {
                 if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]]
@@ -43,13 +47,21 @@ public class MainActivity extends AppCompatActivity {
                     else
                         message = "Red";
 
-                    TextView winnerTextView = findViewById(R.id.winnerTextView);
-                    Button playAgainButton = findViewById(R.id.playAgainButton);
                     winnerTextView.setVisibility(View.VISIBLE);
                     playAgainButton.setVisibility(View.VISIBLE);
                     winnerTextView.setText(message + " has won!");
                     gameOver = true;
+                    counterCount = 0;
                 }
+            }
+
+            // Check for drawn game
+            if (counterCount == 9 && !gameOver) {
+                winnerTextView.setVisibility(View.VISIBLE);
+                playAgainButton.setVisibility(View.VISIBLE);
+                winnerTextView.setText("Draw!");
+                gameOver = true;
+                counterCount = 0;
             }
         }
     }
