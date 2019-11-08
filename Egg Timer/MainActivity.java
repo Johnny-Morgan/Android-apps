01f4caac421e7fa6a292d41f4d80f0c254d54e6f
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-11-08 23:02:40 +0000

    Added CountDownTimer

--- Egg Timer/MainActivity.java
+++ Egg Timer/MainActivity.java
@@ -1,16 +1,24 @@
 package dev.johnmorgan.eggtimer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
-    public String calculateTime(int time){
-        int minutes, seconds;
-        if (time < 60){
+    TextView textView;
+    SeekBar seekBar;
+    CountDownTimer countDownTimer;
+    boolean timerOn = false;
+
+    public String calculateTime(long time) {
+        long minutes, seconds;
+        if (time < 60) {
             minutes = 0;
             seconds = time;
         } else {
@@ -19,15 +27,48 @@ public class MainActivity extends AppCompatActivity {
         }
         return String.format("%d:%02d", minutes, seconds);
     }
+
+    public void startTimer(View view) {
+        if (!timerOn) {
+            Button button = findViewById(R.id.button);
+            button.setText("STOP!");
+            timerOn = true;
+            long time = seekBar.getProgress() * 1000;
+            seekBar.setEnabled(false);
+
+            countDownTimer = new CountDownTimer(time, 1000) {
+                public void onTick(long l) {
+                    textView.setText(calculateTime(l / 1000));
+                }
+
+                public void onFinish() {
+                    seekBar.setEnabled(true);
+                    seekBar.setProgress(30);
+                }
+            }.start();
+
+        } else {
+            timerOn = false;
+            countDownTimer.cancel();
+            Button button = findViewById(R.id.button);
+            button.setText("GO!");
+            seekBar.setEnabled(true);
+            // reset timer to 30 seconds
+            seekBar.setProgress(30);
+            textView.setText(calculateTime(seekBar.getProgress()));
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView textView = findViewById(R.id.timerTextView);
-        SeekBar seekBar = findViewById(R.id.timerSeekBar);
+        textView = findViewById(R.id.timerTextView);
+        seekBar = findViewById(R.id.timerSeekBar);
         seekBar.setMax(600);
         seekBar.setProgress(30);
         textView.setText(calculateTime(seekBar.getProgress()));
+
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
