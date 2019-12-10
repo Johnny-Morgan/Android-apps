d5195eef0a5d06b6d90196ec51d5a4dbc1767641
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-12-10 01:00:58 +0000

    Second commit

--- Donation Data Entry/MainActivity.java
+++ Donation Data Entry/MainActivity.java
@@ -2,9 +2,12 @@ package dev.johnmorgan.donationdataentry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +15,9 @@ public class MainActivity extends AppCompatActivity {
     public void createDonation(View view) {
         EditText donator = findViewById(R.id.donatorEditText);
         EditText date = findViewById(R.id.dateEditText);
-        Log.i("Donator", donator.getEditableText().toString());
-        Log.i("Date", date.getEditableText().toString());
+        Intent intent = new Intent(getApplicationContext(), secondActivity.class);
+        intent.putExtra("donator", donator.getEditableText().toString() + " " + date.getEditableText().toString());
+        startActivity(intent);
     }
 
     @Override
---
+++ Donation Data Entry/SecondActivity.java
@@ -0,0 +1,19 @@
+package dev.johnmorgan.donationdataentry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(intent.getStringExtra("donator"));
+    }
+}
