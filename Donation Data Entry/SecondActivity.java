6d4e23e9c96e8124db244207e337819d69e781ce
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-12-11 23:30:14 +0000

    Added ThirdActivity class

--- Donation Data Entry/SecondActivity.java
+++ Donation Data Entry/SecondActivity.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
+    public void addEntry(View view) {
+        Intent intent = new Intent(getApplicationContext(), ThirdActivity.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ Donation Data Entry/ThirdActivity.java
@@ -0,0 +1,15 @@
+package dev.johnmorgan.donationdataentry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
