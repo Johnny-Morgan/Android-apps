0ea994b5995e7661509fc3ba524ae272c7c2beff
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-12-12 23:21:16 +0000

    Added addProduct method

--- Donation Data Entry/SecondActivity.java
+++ Donation Data Entry/SecondActivity.java
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -13,6 +17,7 @@ public class SecondActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), ThirdActivity.class);
         startActivity(intent);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,8 @@ public class SecondActivity extends AppCompatActivity {
         Intent intent = getIntent();
         TextView textView = findViewById(R.id.textView);
         textView.setText(intent.getStringExtra("donator"));
+
+        Intent intent2 = getIntent();
+        Toast.makeText(this, intent2.getStringExtra("details"), Toast.LENGTH_LONG).show();
     }
 }
--- Donation Data Entry/ThirdActivity.java
+++ Donation Data Entry/ThirdActivity.java
@@ -4,8 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ThirdActivity extends AppCompatActivity {
+
+    String message = "";
+    EditText brand;
+    EditText description;
+    EditText unitWeight;
+    EditText unitsPerCase;
+    EditText totalCases;
+
+    public void addProduct(View view) {
+        brand = findViewById(R.id.brandEditText);
+        description = findViewById(R.id.descriptionEditText);
+        unitWeight = findViewById(R.id.unitWeightEditText);
+        unitsPerCase = findViewById(R.id.unitsPerCaseEditText);
+        totalCases = findViewById(R.id.totalCasesEditText);
+
+        message += "Brand: " + brand.getEditableText().toString()
+                + " Description: " + description.getEditableText().toString()
+                + " Unit pack weight " + unitWeight.getEditableText().toString()
+                + " Pack units per case: " + unitsPerCase.getEditableText().toString()
+                + " Total cases: " + totalCases.getEditableText().toString();
+        Log.i("Details", message);
+
+        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+        intent.putExtra("details", message);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
