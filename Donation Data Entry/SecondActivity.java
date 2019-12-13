f19980e8fdcbfb5f3d9199ad4543620601e10ba9
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-12-13 23:20:58 +0000

    Added addProduct method

--- Donation Data Entry/SecondActivity.java
+++ Donation Data Entry/SecondActivity.java
@@ -1,21 +1,36 @@
 package dev.johnmorgan.donationdataentry;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class SecondActivity extends AppCompatActivity {
+    List<String> products = new ArrayList<>();
+    ListView productListView;
+    ArrayAdapter<String> arrayAdapter;
 
     public void addEntry(View view) {
         Intent intent = new Intent(getApplicationContext(), ThirdActivity.class);
         startActivity(intent);
+    }
+
+    public void addProduct() {
+        productListView = findViewById(R.id.detailsListView);
+        Intent intent2 = getIntent();
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, products);
+        productListView.setAdapter(arrayAdapter);
+
+        if (intent2.getStringExtra("details") != null) {
+            products.add(intent2.getStringExtra("details"));
+        }
     }
 
     @Override
@@ -25,8 +40,6 @@ public class SecondActivity extends AppCompatActivity {
         Intent intent = getIntent();
         TextView textView = findViewById(R.id.textView);
         textView.setText(intent.getStringExtra("donator"));
-
-        Intent intent2 = getIntent();
-        Toast.makeText(this, intent2.getStringExtra("details"), Toast.LENGTH_LONG).show();
+        addProduct();
     }
 }
