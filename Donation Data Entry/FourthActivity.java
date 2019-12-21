0e722dae71379db9448977ae4e9c462221c3283f
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-12-21 23:12:00 +0000

    FourthActivity.java updates
    Removed onRadioButtonClicked method
    Added radiogroup setOnCheckedChangeListener

--- Donation Data Entry/FourthActivity.java
+++ Donation Data Entry/FourthActivity.java
@@ -7,9 +7,13 @@ import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
@@ -17,36 +21,42 @@ public class FourthActivity extends AppCompatActivity {
 
     EditText frozenUntilDate;
     EditText location;
+    Date today = Calendar.getInstance().getTime();
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy");
+    String formattedDate = sdf.format(today);
 
     public void addEntryButtonClicked(View view) {
         Intent intent = getIntent();
         String message = intent.getStringExtra("details");
         frozenUntilDate = findViewById(R.id.frozenEditText);
-        message += " BB: " + frozenUntilDate.getEditableText().toString();
+        message += " FROZEN ON " + formattedDate + " BB: " + frozenUntilDate.getEditableText().toString();
         Log.i("message", message);
         Intent intent2 = new Intent(getApplicationContext(), SecondActivity.class);
         intent.putExtra("details", message.toUpperCase());
         startActivity(intent2);
     }
 
-    public void onRadioButtonClicked(int button) {
-        switch (button) {
-            case R.id.radio_ambient:
-                location = findViewById(R.id.locationEditText);
-                location.setEnabled(true);
-                break;
-            case R.id.radio_fridge:
-                break;
-            case R.id.radio_freezer:
-                frozenUntilDate = findViewById(R.id.frozenEditText);
-                frozenUntilDate.setEnabled(true);
-                break;
-        }
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fourth);
+        frozenUntilDate = findViewById(R.id.frozenEditText);
+        location = findViewById(R.id.locationEditText);
+        RadioGroup rg = findViewById(R.id.locationRadioGroup);
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.radio_freezer) {
+                    frozenUntilDate.setEnabled(true);
+                    location.setEnabled(false);
+                } else if (checkedId == R.id.radio_ambient) {
+                    location.setEnabled(true);
+                    frozenUntilDate.setEnabled(false);
+                } else {
+                    frozenUntilDate.setEnabled(false);
+                    location.setEnabled(false);
+                }
+            }
+        });
     }
 }
