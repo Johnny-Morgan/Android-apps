9da0714bd3caff28f5ca6a60fd28e4ef362ae561
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-12-24 22:54:37 +0000

    Added DatePickerDialog to MainActivity

--- Donation Data Entry/MainActivity.java
+++ Donation Data Entry/MainActivity.java
@@ -2,27 +2,71 @@ package dev.johnmorgan.donationdataentry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText donator;
+    EditText dateEditText;
+    final Calendar myCalendar = Calendar.getInstance();
+
     public void createDonation(View view) {
-        EditText donator = findViewById(R.id.donatorEditText);
-        EditText date = findViewById(R.id.dateEditText);
-        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-        intent.putExtra("donator", donator.getEditableText().toString() + " " + date.getEditableText().toString());
-        startActivity(intent);
+        if(donator.getEditableText().toString().equals("") || dateEditText.getEditableText().toString().equals("")){
+            Toast.makeText(this, "Donator and Date are required", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+            intent.putExtra("donator", donator.getEditableText().toString() + " " + dateEditText.getEditableText().toString());
+            startActivity(intent);
+        }
+    }
+
+    public void updateLabel() {
+        String myFormat = "dd/MM/yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.getDefault());
+        dateEditText.setText(sdf.format(myCalendar.getTime()));
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        donator = findViewById(R.id.donatorEditText);
+        dateEditText = findViewById(R.id.dateEditText);
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+
+        dateEditText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(MainActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
     }
 }
