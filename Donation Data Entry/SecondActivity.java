90f3572f137c781b75e48fec9aa404acd840d214
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2020-01-06 23:50:53 +0000

    SecondActivity updates

--- Donation Data Entry/SecondActivity.java
+++ Donation Data Entry/SecondActivity.java
@@ -5,43 +5,48 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SecondActivity extends AppCompatActivity {
+
     List<String> products = new ArrayList<>();
     ListView productListView;
     ArrayAdapter<String> arrayAdapter;
-
-    public void addEntry(View view) {
-        Intent intent = new Intent(getApplicationContext(), ThirdActivity.class);
-        startActivity(intent);
-    }
-
-    public void addProduct() {
-        productListView = findViewById(R.id.detailsListView);
-        Intent intent2 = getIntent();
-        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, products);
-        productListView.setAdapter(arrayAdapter);
-
-        if (intent2.getStringExtra("details") != null) {
-            products.add(intent2.getStringExtra("details"));
-            Log.i("w", intent2.getStringExtra("details"));
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Intent intent = getIntent();
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(intent.getStringExtra("donator"));
-        addProduct();
+        productListView = findViewById(R.id.detailsListView);
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, products);
+
+        Button addEntryButton = findViewById(R.id.button);
+        addEntryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), FourthActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                String result = data.getStringExtra("details");
+                products.add(result);
+                productListView.setAdapter(arrayAdapter);
+            }
+        }
     }
 }
