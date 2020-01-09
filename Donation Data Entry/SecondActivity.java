a8793dfd516812afb8d0099c6fc71ca00c0b336c
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2020-01-09 23:54:24 +0000

    Code to write products to a file

--- Donation Data Entry/SecondActivity.java
+++ Donation Data Entry/SecondActivity.java
@@ -1,5 +1,6 @@
 package dev.johnmorgan.donationdataentry;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,13 +8,18 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -32,8 +38,17 @@ public class SecondActivity extends AppCompatActivity {
         addEntryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), FourthActivity.class);
+                Intent intent = new Intent(getApplicationContext(), ThirdActivity.class);
                 startActivityForResult(intent, 1);
+            }
+        });
+
+        Button finishButton = findViewById(R.id.finishButton);
+        finishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // test - check if first item in products is written to file
+                writeToFile(products.get(0), "C:\\Users\\jwjmo\\AndroidStudioProjects\\DonationDataEntry\\app\\output", getApplicationContext());
             }
         });
     }
@@ -47,6 +62,16 @@ public class SecondActivity extends AppCompatActivity {
                 products.add(result);
                 productListView.setAdapter(arrayAdapter);
             }
+        }
+    }
+
+    public static void writeToFile(final String fileContents, String fileName,  Context context) {
+        try {
+            FileWriter out = new FileWriter(new File(context.getFilesDir(), fileName));
+            out.write(fileContents);
+            out.close();
+        } catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
         }
     }
 }
